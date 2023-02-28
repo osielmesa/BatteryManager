fa4d4fc7deb33fc85d0d758b792b67ce0e49d928
Osiel Lima Díaz <osielmesa@gmail.com>
2023-02-28 22:41:27 +0100

    project init

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  View,
+} from 'react-native';
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+      />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
