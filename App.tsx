2c1866b4b92b5c472f7ee42368b819d2f9a66270
Osiel Lima Díaz <osielmesa@gmail.com>
2023-03-01 11:57:16 +0100

    Use the native module from react-native

--- App.tsx
+++ App.tsx
@@ -6,27 +6,30 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, StatusBar, useColorScheme, View, NativeModules, Button, StyleSheet } from 'react-native';
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const { BatteryModule } = NativeModules; // here we use the name returned in our getName method of BatteryModule java class
+  const onPress = () => {
+    BatteryModule.nativeLogger('RN to Android', 'This is a log comming from JS');
+  };
 
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-      />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View>
+        <Button title="Click to log data in android console!" color="#303F9F" onPress={onPress} />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
